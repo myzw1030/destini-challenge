7f6a94346d64ca93aea401dc3ea3b669f808782f
tommy <tmtmtoa@gmail.com>
2022-08-02 22:47:28 +0900

    restart

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -54,11 +54,28 @@ class StroyBrain {
 
   int storyNumber = 0;
 
-  void nextStory(int choiceNumber) {}
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 2 && storyNumber == 0) {
+      storyNumber = 1;
+    } else if (choiceNumber == 1 && storyNumber == 0) {
+      storyNumber = 2;
+    } else if (choiceNumber == 1 && storyNumber == 1) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 1) {
+      storyNumber = 3;
+    } else if (choiceNumber == 2 && storyNumber == 2) {
+      storyNumber = 4;
+    } else if (choiceNumber == 1 && storyNumber == 2) {
+      storyNumber = 5;
+    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+      restart();
+    }
+  }
 
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+  // storyNumberをリセット
+  void restart() {
+    storyNumber = 0;
+  }
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 
